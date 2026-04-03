30dfe2018a12b19ec8425faabb2fad5f95f75050
payalptoliya416 <payal.rising416@gmail.com>
2026-04-03 16:58:55 +0530

    chnages

--- src/site/common/SiteFooter.tsx
+++ src/site/common/SiteFooter.tsx
@@ -2,21 +2,23 @@
 import logo from "@/assets/logo.png";
 import footerlogo from "@/assets/site/footer-logo.png";
 import footerbg from "@/assets/site/footerbg.png";
-import { FaFacebookF, FaPaperPlane, FaYoutube } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
+import l1 from "@/assets/site/l1.svg";
+import l2 from "@/assets/site/l2.svg";
+import l3 from "@/assets/site/l3.svg";
+import l4 from "@/assets/site/l4.svg";
 
 export default function SiteFooter() {
   const socials = [
-    { icon: <FaFacebookF size={14} />, link: "https://facebook.com" },
-    { icon: <FaYoutube size={14} />, link: "https://youtube.com" },
-    { icon: <FaXTwitter size={14} />, link: "https://twitter.com" },
-    { icon: <FaPaperPlane size={14} />, link: "https://t.me" },
+    { icon: l1, link: "https://facebook.com" },
+    { icon: l2, link: "https://youtube.com" },
+    { icon: l3, link: "https://twitter.com" },
+    { icon: l4, link: "https://t.me" },
   ];
   return (
     <footer className="relative bg-[#13192B] pt-[84px] bg-no-repeat"  style={{ backgroundImage: `url(${footerbg})`, backgroundSize: "100% 100%" }}>
       <div className="absolute -top-10 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[700px] lg:max-w-[800px] xl:max-w-[1030px]">
         <div className="bg-[#25C866] rounded-[40px] py-10 sm:py-[60px] px-6 text-center shadow-lg">
-          <h2 className="text-2xl md:text-[38px] font-extrabold text-[#262732] mb-[30px]">
+          <h2 className="text-2xl md:text-[38px] leading-[38px] font-extrabold text-[#262732] mb-[30px]">
             Take Control of Your{" "}
             <span className="text-white">Crypto Today</span>
           </h2>
@@ -62,7 +64,7 @@ export default function SiteFooter() {
                 rel="noopener noreferrer"
                 className="w-[28px] h-[28px] rounded-full bg-[#FFFFFF0D] text-[#E9E8E8] flex items-center justify-center hover:bg-white/20 hover:text-white transition"
               >
-                {item.icon}
+                <img src={item.icon} alt="" />
               </a>
             ))}
           </div>
