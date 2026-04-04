22fe329af4691ba0fc72bb78e5336ebfb482fb32
payalptoliya416 <payal.rising416@gmail.com>
2026-04-04 10:57:31 +0530

    leading

--- src/site/pages/home/HeroSection.tsx
+++ src/site/pages/home/HeroSection.tsx
@@ -18,7 +18,7 @@ function Home() {
           <div className="grid grid-cols-12 text-center lg:text-left">
             <div className="col-span-12 lg:col-span-7 xl:col-span-6 text-white">
               <h1
-                className="text-4xl md:text-[56px] font-extrabold lg:leading-[74px] mb-[15px]">
+                className="text-4xl md:text-[56px] font-extrabold leading-[42px] md:leading-[74px] mb-[15px]">
                 Secure Your Crypto Assets with{" "}
                 <span className="text-[#25C866]">
                   Fortress Level Protection
