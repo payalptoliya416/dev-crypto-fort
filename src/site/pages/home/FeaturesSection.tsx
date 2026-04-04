c9b51983cee59d3f08be5ca0455f94cb35171519
payalptoliya416 <payal.rising416@gmail.com>
2026-04-04 09:48:59 +0530

    code

--- src/site/pages/home/FeaturesSection.tsx
+++ src/site/pages/home/FeaturesSection.tsx
@@ -31,7 +31,7 @@ export default function FeaturesSection() {
           {features.map((item, i) => (
             <div
               key={i}
-              className="bg-[#F4F4F566] rounded-[30px] border border-[#E9E9EB] p-[30px] text-center
+              className="bg-[#F4F4F566] rounded-[30px] border border-[#E9E9EB] py-[40px] px-[30px] text-center
               transition-all duration-300 hover:shadow-lg hover:-translate-y-1"
             >
               <div className="w-[70px] sm:w-[84px] h-[70px] sm:h-[84px] mx-auto mb-[30px] rounded-[20px] bg-[#25C8661A] flex items-center justify-center">
